fd1b29c0a24c233c42dabc931b9f67e791752e5f
Aayush dusane <aayushdusane8030@gmail.com>
2026-05-13 12:11:16 +0530

    Additional features

--- backend/src/routes/repoRoutes.js
+++ backend/src/routes/repoRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
 const {
   createRepo,
   getRepos,
+  getReposByProject,
+  getRepoById,
+  updatePort,
   updateEnvironment
 } = require("../controllers/repoController");
 
@@ -17,6 +19,15 @@ router.post("/create", protect, createRepo);
 
 // get repositories
 router.get("/", protect, getRepos);
+
+// get project repositories
+router.get("/project/:projectId", protect, getReposByProject);
+
+// get single repository
+router.get("/:id", protect, getRepoById);
+
+// update port
+router.put("/port/:id", protect, updatePort);
 
 
 // update environment
